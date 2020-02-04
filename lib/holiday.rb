98939947692267be7d98cabfda257de9558e9348
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-04 02:30:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
     new_array = []
     
-    
+    holiday_hash.each do |season, holiday|
+      holiday_hash.each do |halliday
 
 end
 
