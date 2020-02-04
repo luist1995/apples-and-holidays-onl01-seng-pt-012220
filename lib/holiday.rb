ae7a13ce6c3f90bf7b861563f67e9517aff098a3
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-04 02:17:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,9 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter]
+  holiday_hash[:winter].collect do |holiday, array|
+    array
+  end
   
 
 end
